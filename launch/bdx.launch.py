0b22ff7e81d04a259117a5b0a1ddcf08e26fab54
Jaime <jaime.machuca@toro.com>
2025-03-07 20:37:11 +0000

    use puddleduck_control node instead of the old joystick and serial joint publishers.

--- launch/bdx.launch.py
+++ launch/bdx.launch.py
@@ -20,13 +20,13 @@ def generate_launch_description():
     launch_rviz = LaunchConfiguration('launch_rviz')
     use_joy_bridge = LaunchConfiguration('use_joy_bridge')
 
-    joint_state_package_share = get_package_share_directory('puddleduck_control')
-    joint_state_config_file = os.path.join(joint_state_package_share, 'config', 'joint_config.yaml')
+    puddleduck_control_package_share = get_package_share_directory('puddleduck_control')
+    puddleduck_control_config_file = os.path.join(puddleduck_control_package_share, 'config', 'puddleduck_control.yaml')
 
-    if not os.path.exists(joint_state_config_file):
-        print("Config file not found: {}".format(joint_state_config_file))
+    if not os.path.exists(puddleduck_control_config_file):
+        print("Config file not found: {}".format(puddleduck_control_config_file))
     else:
-        print("Using config file: {}".format(joint_state_config_file))
+        print("Using config file: {}".format(puddleduck_control_config_file))
 
 
     # Read the URDF file
@@ -39,16 +39,6 @@ def generate_launch_description():
             'launch_rviz',
             default_value='false',
             description='Set to "true" to launch RViz')
-  
-    declare_joint_state_publish_rate_cmd = DeclareLaunchArgument(
-            'joint_state_publish_rate',
-            default_value='10',
-            description='Rate at which to publish joint states')
-    
-    declare_motor_command_publish_rate_cmd = DeclareLaunchArgument(
-            'motor_command_publish_rate',
-            default_value='100',
-            description='Rate at which to send motor commands')
 
     # Pass the robot_description parameter
     robot_state_publisher_node = Node(
@@ -56,34 +46,6 @@ def generate_launch_description():
         executable='robot_state_publisher',
         output='screen',
         parameters=[{'robot_description': robot_desc}]
-    )
-# Smoothing Parameters
-# smoothing_alpha
-#   Used only with the EMA method, this value (typically between 0.0 and 1.0) determines the weight given to the new command versus the previous command.
-#   A higher alpha (closer to 1) means the new input is used more, resulting in less smoothing (i.e., faster response but more abrupt changes).
-#   A lower alpha (closer to 0) means the previous value dominates, leading to smoother but slower changes.
-# smoothing_max_delta
-#   When using the ramp method, this parameter defines the maximum allowed change in the command value per update cycle. It effectively caps the acceleration of the actuator’s movement.
-#   A smaller value means the command can only change slowly, ensuring very smooth transitions.
-#   A larger value allows faster changes, which might feel more responsive but can also lead to jerky motion if the input signal is noisy.
-
-    joint_state_publisher_node = Node(
-        package='puddleduck_control',
-        executable='serial_joint_state_publisher_node',
-        name='serial_joint_state_publisher',
-        output='screen',
-        parameters=[joint_state_config_file,
-            {
-                "smoothing_alpha": 0.1,         # Used for "ema" smoothing
-                "smoothing_max_delta": 0.3,      # Used for "ramp" smoothing
-                "head_pitch_mix_positive_threshold": 0.2,
-                "head_pitch_mix_negative_threshold": -0.13,
-                "neck_pitch_mix_scale_positive": 0.7,
-                "neck_pitch_mix_scale_negative": 1.3,
-                "neck_pitch_offset": 0.45
-            }],
-        # Uncomment the next line to set the log level to debug:
-        # arguments=['--ros-args', '--log-level', 'debug']
     )
 
     # Conditionally launch RViz if 'launch_rviz' is true
@@ -122,23 +84,23 @@ def generate_launch_description():
         }]
     )
 
-    joy_serial_bridge_node = Node(
+    puddleduck_control_node = Node(
         package='puddleduck_control',
-        executable='joy_serial_bridge_node',
-        name='joy_serial_bridge_node',
+        executable='puddleduck_control_node',
+        name='puddleduck_control',
         output='screen',
-        condition=IfCondition(use_joy_bridge)
+        parameters=[puddleduck_control_config_file],
+        # condition=IfCondition(use_joy_bridge)
+        # Uncomment the next line to set the log level to debug:
+        # arguments=['--ros-args', '--log-level', 'debug']
     )
 
     return LaunchDescription([
         declare_foxglove_bridge_arg,
         declare_joy_bridge_arg,
         declare_rviz_cmd,
-        declare_joint_state_publish_rate_cmd,
-        declare_motor_command_publish_rate_cmd,
         robot_state_publisher_node,
-        joint_state_publisher_node,
+        puddleduck_control_node,
         rviz_node,
-        foxglove_bridge_node,
-        joy_serial_bridge_node
+        foxglove_bridge_node
     ])
